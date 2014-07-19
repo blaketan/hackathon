b7f73b4b786da4dbfbc49053c5fd95b0403482a9
blaketan <koonchong@gmail.com>
2014-07-18 19:30:43 -0700

    main files

---
+++ thegame.py
@@ -0,0 +1,41 @@
+import pygame
+import random
+
+#classes
+
+class Player(x = 400,y = 300):
+# this class refers to the player object
+	def __init__(self,x,y)
+
+
+
+
+
+
+
+
+
+# initialize pygame
+pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode([screen_width, screen_height])
+done = False
+
+#main game loop
+player()
+
+while not done:
+	for event in pygame.event.get()
+	    if event.type == pygame.QUIT:
+            done = True
+        elif event = pygame.KEYUP
+
+
+
+
+
+
+
+
+pygame.quit()
