48b9371b1fd1278c018cceb75222fb7a3f4a3898
blaketan <koonchong@gmail.com>
2014-07-18 23:41:34 -0700

    FINALLLLL

--- final.py
+++ final.py
@@ -85,7 +85,7 @@ def main():
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    pygame.display.set_caption('Test')
+    pygame.display.set_caption('Lamplit')
     BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
 
     mainBoard = generateBoard()
